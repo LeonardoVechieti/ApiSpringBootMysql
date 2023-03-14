a4f57afaaa760fe596ea880ecb0bbef552c8b2de
LeonardoVechieti <92764500+LeonardoVechieti@users.noreply.github.com>
2023-03-14 19:09:50 -0300

    Init project

---
+++ src/test/java/br/com/leonardovechieti/apispringmysql/ApiSpringMysqlApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.leonardovechieti.apispringmysql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiSpringMysqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
